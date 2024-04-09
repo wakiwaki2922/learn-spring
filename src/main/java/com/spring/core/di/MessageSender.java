09c0aded68275773e6361b96db15ab1f62d88089
wakiwaki2922 <wyzeman.2922@gmail.com>
2024-04-09 16:18:46 +0700

    4th commit

--- src/main/java/com/spring/core/di/MessageSender.java
+++ src/main/java/com/spring/core/di/MessageSender.java
@@ -9,18 +9,48 @@ public class MessageSender {
 
     //    private SMSService smsService;
     //  private EmailService emailService;
+
+    @Autowired
+    @Qualifier("emailService")
     private MessageService messageService;
 
     @Autowired
-    public MessageSender(@Qualifier("emailService") MessageService messageService) {
+    private MessageService smsService;
+
+//    //@Autowired
+//    public MessageSender(@Qualifier("emailService") MessageService messageService) {
+////        this.smsService = smsService;
+////        this.emailService = emailService;
+//        this.messageService = messageService;
+//        System.out.println("Constructor based dependency injection 1");
+//    }
+//
+//    @Autowired
+//    public MessageSender(@Qualifier("emailService") MessageService messageService,
+//                         MessageService smsService) {
+////        this.smsService = smsService;
+////        this.emailService = emailService;
+//        this.messageService = messageService;
 //        this.smsService = smsService;
-//        this.emailService = emailService;
-        this.messageService = messageService;
-    }
+//        System.out.println("Constructor based dependency injection 2");
+//    }
+
+//    @Autowired
+//    public void setMessageService(@Qualifier("emailService") MessageService messageService) {
+//        this.messageService = messageService;
+//        System.out.println("Setter based dependency injection 1");
+//    }
+//
+//    @Autowired
+//    public void setSmsService(MessageService smsService) {
+//        this.smsService = smsService;
+//        System.out.println("Setter based dependency injection 2");
+//    }
 
     public void sendMessage(String message) {
 //        this.smsService.sendMessage(message);
 //        this.emailService.sendMessage(message);
         this.messageService.sendMessage(message);
+        this.smsService.sendMessage(message);
     }
 }
--- src/main/java/com/spring/core/di/SMSService.java
+++ src/main/java/com/spring/core/di/SMSService.java
@@ -2,7 +2,7 @@ package com.spring.core.di;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("smsService")
 public class SMSService implements MessageService {
 
     @Override
